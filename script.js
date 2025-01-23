2ac5a042f88600e6c8a8ddc4adc5bcba625685e1
IbrahimMurad <ibrahimmorad31@gmail.com>
2025-01-23 17:21:51 +0200

    Document and add comments to script file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get references to DOM elements
 const billInput = document.getElementById("bill");
 const customTipInput = document.getElementById("custom-tip");
 const tipButtons = document.querySelectorAll(".tip-percent");
@@ -10,6 +11,7 @@ const peopleError = document.getElementById("people-number-error-message");
 const totalResult = document.getElementById("total-result");
 const tipAmountResult = document.getElementById("tip-amount-result");
 
+// Object to store input values
 const inputFields = {
   bill: 0,
   tip: 0,
@@ -48,37 +50,54 @@ function reset() {
   tipAmountResult.textContent = "$0.0";
 }
 
+// Event listener for bill input to check the input value whenever it changes
 billInput.addEventListener("input", () => {
+  // covert from string to number
   const billValue = Number(billInput.value);
+
+  // handle negaitve values
   if (billValue < 0) {
     billError.textContent = "Can't be negative";
     billInput.classList.add("error");
     return;
   }
+
+  // save the value in the inputFields object
   billInput.classList.remove("error");
   inputFields.bill = parseFloat(billInput.value);
+
+  // calculate tip if all inputs are valid
   calculateTip();
 });
 
+// Event listeners for tip inputs [type="button"]
 tipButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    // deactivate other tip buttons
     tipButtons.forEach((button) => {
       button.classList.remove("active");
     });
     button.classList.add("active");
     customTipInput.value = "";
+
+    // save the value in the inputFields object
     inputFields.tip = parseFloat(button.value);
+
+    // calculate tip if all inputs are valid
     calculateTip();
   });
 });
 
+// Event listener for custom tip input to check the input value whenever it changes
 customTipInput.addEventListener("input", () => {
   const tipValue = Number(customTipInput.value);
 
+  // deactivate other tip buttons
   tipButtons.forEach((button) => {
     button.classList.remove("active");
   });
 
+  // handle invalid values
   if (tipValue < 0) {
     tipError.textContent = "Can't be negative";
     customTipInput.classList.add("error");
@@ -91,14 +110,22 @@ customTipInput.addEventListener("input", () => {
     return;
   }
 
+  // ensure there are no error messages
   customTipInput.classList.remove("error");
   tipError.textContent = "";
+
+  // save the value in the inputFields object
   inputFields.tip = tipValue;
+
+  // calculate tip if all inputs are valid
   calculateTip();
 });
 
+// Event listener for number of people input
 peopleInput.addEventListener("input", () => {
   const peopleValue = Number(peopleInput.value);
+
+  // handle invalid values
   if (peopleValue < 0) {
     peopleError.textContent = "Can't be negative";
     peopleInput.classList.add("error");
@@ -116,31 +143,49 @@ peopleInput.addEventListener("input", () => {
     peopleInput.classList.add("error");
     return;
   }
+
+  // ensure there are no error messages
   peopleInput.classList.remove("error");
   peopleError.textContent = "";
+
+  // save the value in the inputFields object
   inputFields.people = peopleValue;
+
+  // calculate tip if all inputs are valid
   calculateTip();
 });
 
+// Event listener for reset button
 resetButton.addEventListener("click", reset);
 
+// Function to update the result display
 function updateResult(result) {
   tipAmountResult.textContent = result.tipPerPerson;
   totalResult.textContent = result.totalPerPerson;
 }
 
+// Function to calculate the tip and total amounts
 function calculateTip() {
+  // check if all inputs are valid
   if (inputFields.people === 0) {
     return;
   }
+
+  // activate the reset button
   resetButton.classList.add("active");
+
+  // calculate
   const tipAmount = inputFields.bill * (inputFields.tip / 100);
   const totalAmount = inputFields.bill + tipAmount;
   const tipPerPerson = tipAmount / inputFields.people;
   const totalPerPerson = totalAmount / inputFields.people;
+
+  // store the calculated values in the currency format
   const result = {
     tipPerPerson: "$" + tipPerPerson.toFixed(2),
     totalPerPerson: "$" + totalPerPerson.toFixed(2),
   };
+
+  // update the result display
   updateResult(result);
 }
